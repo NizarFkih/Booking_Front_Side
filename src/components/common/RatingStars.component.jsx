1889d7b3331b1c82f3ce7a3a650d3d6e6c2d24cd
NizarFkih <elfekihnizar7@gmail.com>
2024-02-19 16:11:39 +0100

    Add RatingStars component

---
+++ src/components/common/RatingStars.component.jsx
@@ -0,0 +1,21 @@
+import styles from "./styles/RatingStars.module.scss";
+import { IoStarOutline, IoStar } from "react-icons/io5";
+const RatingStars = ({ rate = 3 }) => {
+  const items = Array.from({ length: 5 }, (_, index) => index + 1);
+  return (
+    <span className={styles.ratingStars}>
+      {items.map((item, index) => {
+        console.log(item);
+        if (item <= rate) {
+          return <IoStar key={index} className={styles.ratingStars__icon} />;
+        } else {
+          return (
+            <IoStarOutline key={index} className={styles.ratingStars__icon} />
+          );
+        }
+      })}
+    </span>
+  );
+};
+
+export default RatingStars;
