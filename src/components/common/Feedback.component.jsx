b71d3afd450525f9ec8068116431cc40a34f8104
NizarFkih <elfekihnizar7@gmail.com>
2024-02-19 03:50:33 +0100

    In this commit, we create a feedback component -This component will be used as its name means to display different users' feedback

---
+++ src/components/common/Feedback.component.jsx
@@ -0,0 +1,20 @@
+import UserProfileAvatar from "./UserProfileAvatar.component";
+import styles from "./styles/Feedback.module.scss";
+
+const Feedback = ({ data }) => {
+  return (
+    <figure className={styles.feedback}>
+      <blockquote className={styles.feedback__text}>{data.text}</blockquote>
+      <div className={styles.feedback__overview}>
+        <UserProfileAvatar img={data.avatar ? data.avatar : null} />
+        <div className={styles.feedback__user}>
+          <p className={styles.feedback__username}>{data.username}</p>
+          <p className={styles.feedback__createdAt}>{data.createdAt}</p>
+        </div>
+        <span className={styles.feedback__rate}>{data.rate}</span>
+      </div>
+    </figure>
+  );
+};
+
+export default Feedback;
