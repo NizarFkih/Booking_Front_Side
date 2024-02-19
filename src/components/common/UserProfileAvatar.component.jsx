810b5c55f063867deca511cd7edfa9537f0dc94c
NizarFkih <elfekihnizar7@gmail.com>
2024-02-19 03:00:09 +0100

    In this commit, we create the UserProfileAvatar component -This component will be to display the picture of the current user. -Used in the future also to display which users give certain feedback and users who make recommendations. -I set a default avatar for new users who have not added a profile picture yet.

---
+++ src/components/common/UserProfileAvatar.component.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles/UserProfileAvatar.module.scss";
+import avatar from "./../../assets/avatar.png";
+const UserProfileAvatar = ({ img, customStyle = {} }) => {
+  return (
+    <div className={styles.avatar} style={{ ...customStyle }}>
+      <img src={img?img:avatar} alt="avatar" className={styles.avatar__img} />
+    </div>
+  );
+};
+
+export default UserProfileAvatar;
