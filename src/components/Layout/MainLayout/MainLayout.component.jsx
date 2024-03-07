1d1940a3abdb69e20a8df20287cf88a0d25421c4
NizarFkih <elfekihnizar7@gmail.com>
2024-03-07 16:23:50 +0100

    fix redirection extention error Add home page

--- src/components/Layout/MainLayout/MainLayout.component.jsx
+++ src/components/Layout/MainLayout/MainLayout.component.jsx
@@ -3,13 +3,15 @@ import SideBar from "./../../SideBar/SideBar.component";
 import styles from "./MainLayout.module.scss";
 import HotelDetails from "../../../pages/HotelDetails/HotelDetails.component";
 import PageNotFound from "../../../pages/pageNotFound/PageNotFound.component";
+import Home from "../../../pages/Home/Home.component";
 const MainLayout = () => {
   return (
     <main className={styles.main}>
       <SideBar />
       <Routes>
+      <Route path="/" index element={<Home />} />
         <Route path="/hotel" element={<HotelDetails />} />
-        <Route path="*" element={<PageNotFound/>} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </main>
   );
---
+++ src/pages/Home/Home.component.jsx
@@ -0,0 +1,21 @@
+import styles from './Home.module.scss';
+import image from './../../assets/image.png'
+const Home = () => {
+    return (<div className={styles.container}>
+        <div className={styles.container__mainContent}>
+            <div className={styles.text}>
+                <h1 className={styles.text__primary}><span>Booking</span> <span>All</span> <span>In</span> <span>One</span></h1>
+                <p className={styles.text__secondary}>Lorem, ipsum dolor sit amet consectetur adipisiing elit. Are rem, consequatur eum? es dell Minima reprehenderit. consequatur eum? es dell Minima reprehenderit.</p>
+            </div>
+            <div className={styles.actions}>
+                <a href="/register" className={`${styles.btn} ${styles.btn___register}`}>register</a>
+                <a href="/login" className={`${styles.btn} ${styles.btn___login}`}>login</a></div>
+
+        </div>
+        <div className={styles.container__image}>
+            <img className={styles.img} src={image} alt="" />
+        </div>
+    </div>);
+}
+
+export default Home;
